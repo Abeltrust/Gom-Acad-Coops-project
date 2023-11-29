b0e406bc37e4bca1861f68a5f5a9a18fc8bbbf81
Duba Abel Bitrus <87368904+Abeltrust@users.noreply.github.com>
2023-11-29 10:43:33 -0800

    composer Update

--- resources/views/pages/users/profile.blade.php
+++ resources/views/pages/users/profile.blade.php
@@ -6,35 +6,41 @@
         <div class="text-center">
             <img src="avatar-6.jpg" class="rounded-circle" style="width: 150px; height: 150px;" alt="User Image">
             <h3>User's Name</h3>
-            <div class="mt-2">
+            <!-- <div class="mt-2">
                 <span class="badge bg-success py-2">Verified</span>
                 <span class="badge bg-warning py-2 text-dark">Pending</span>
-            </div>
-            <div class="mt-2">
-               <span class="badge text-dark">your profile is 50% complete</span>
-               <a class="btn btn-primary py-1" href="{{route('membership')}}">Complete Profile</a>
-            </div>
+            </div> -->
+           
         </div>
         
         <div class="mt-4">
-            <h4>User Information</h4>
-            <table class="table">
+           <div class="d-flex justify-content-between">
+             <h4>Requisition Information</h4>  
+             <div class="mt-2">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ApplicationModal">Apply for Loan</button>
+             </div>
+           </div>
+            <table class="table nowrap">
+                <thead>
+                    <tr>
+                        <th>Item </th>
+                        <th>Qnty</th>
+                        <th>Price</th>
+                        <th>Amount</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
                 <tbody>
                     <tr>
-                        <td>Name</td>
-                        <td>User's Name</td>
+                        <td>Rice</td>
+                        <td>2 bags</td>
+                        <td>&#8358;20,000</td>
+                        <td>&#8358;40,000</td>
+                        <td><span class="bg-success text-light p-1 rounded"> Approved </span></td>
                     </tr>
-                    <tr>
-                        <td>Email</td>
-                        <td>user@example.com</td>
-                    </tr>
-                    <tr>
-                        <td>Location</td>
-                        <td>City, Country</td>
-                    </tr>
-                    <!-- Add more rows for additional information -->
                 </tbody>
             </table>
         </div>
     </div>
+    @include('pages.users.modal')
     @endsection
